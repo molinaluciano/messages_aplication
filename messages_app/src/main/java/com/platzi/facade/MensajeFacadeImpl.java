b8acab9b296209689da116ef52e67f5613d950fc
Luciano Molina <lmolina.s@cocha.com>
2021-05-27 23:22:21 -0400

    Crud basico with aplication service via console

--- messages_app/src/main/java/com/platzi/facade/MensajeFacadeImpl.java
+++ messages_app/src/main/java/com/platzi/facade/MensajeFacadeImpl.java
@@ -5,7 +5,12 @@
  */
 package com.platzi.facade;
 
+import com.platzi.config.Conection;
 import com.platzi.model.Mensaje;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -15,22 +20,111 @@ public class MensajeFacadeImpl implements MensajeFacade {
 
     @Override
     public void crearMensajeDB(Mensaje mensaje) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Conection db_connect = new Conection();
+       try(Connection conexion = db_connect.get_connection()){
+            PreparedStatement ps = null;
+
+            try{
+                String query = "INSERT INTO mensajes ( MENSAJE, AUTOR_MENSAJE, FECHA_MENSAJE) VALUES ( ?, ?, ?)";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2, mensaje.getAutor_mensaje());
+                ps.setString(3, mensaje.getFecha_mensaje());
+                System.out.println("---QUERY TO EXECUTE---");
+                System.out.println(ps);
+                ps.executeUpdate();
+               
+                System.out.println("QUERY EJECUTADA CORRECTAMENTE");
+            }
+            catch(SQLException ex){
+                System.out.println(ex);
+            }
+       }
+       catch(Exception e)  {
+          System.out.println(e);
+        }
     }
 
     @Override
     public void leerMensajesDB() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         Conection db_connect = new Conection();
+       try(Connection conexion = db_connect.get_connection()){
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            try{
+                String query = "SELECT * FROM mensajes";
+                ps = conexion.prepareStatement(query);
+                
+                System.out.println("---QUERY TO EXECUTE---");
+                System.out.println(ps);
+                rs = ps.executeQuery();
+               while(rs.next()){
+                  System.out.println("id: " + rs.getInt("id_mensaje"));
+                  System.out.println("mensaje: " + rs.getString("mensaje"));
+               }
+             
+                System.out.println("QUERY EJECUTADA CORRECTAMENTE");
+            }
+            catch(SQLException ex){
+                System.out.println(ex);
+            }
+       }
+       catch(Exception e)  {
+          System.out.println(e);
+        }
     }
 
     @Override
     public void borrarMensajeDB(int id_mensaje) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Conection db_connect = new Conection();
+       try(Connection conexion = db_connect.get_connection()){
+            PreparedStatement ps = null;
+
+            try{
+                String query = "DELETE FROM mensajes WHERE id_mensaje = ?";
+                ps = conexion.prepareStatement(query);
+                ps.setInt(1, id_mensaje);
+                
+                System.out.println("---QUERY TO EXECUTE---");
+                System.out.println(ps);
+                ps.executeUpdate();
+               
+                System.out.println("QUERY EJECUTADA CORRECTAMENTE");
+            }
+            catch(SQLException ex){
+                System.out.println(ex);
+            }
+       }
+       catch(Exception e)  {
+          System.out.println(e);
+        }
     }
 
     @Override
     public void actualizarMensajeDB(Mensaje mensaje) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Conection db_connect = new Conection();
+        try(Connection conexion = db_connect.get_connection()){
+             PreparedStatement ps = null;
+
+             try{
+                 String query = "UPDATE mensajes set mensaje = ? WHERE id_mensaje = ?";
+                 ps = conexion.prepareStatement(query);
+                 ps.setString(1, mensaje.getMensaje());
+                 ps.setInt(2, mensaje.getId_mensaje());
+
+                 System.out.println("---QUERY TO EXECUTE---");
+                 System.out.println(ps);
+                 ps.executeUpdate();
+
+                 System.out.println("QUERY EJECUTADA CORRECTAMENTE");
+             }
+             catch(SQLException ex){
+                 System.out.println(ex);
+             }
+        }
+        catch(Exception e)  {
+          System.out.println(e);
+        }
     }
     
 }
--- messages_app/src/main/java/com/platzi/service/MensajeService.java
+++ messages_app/src/main/java/com/platzi/service/MensajeService.java
@@ -5,13 +5,54 @@
  */
 package com.platzi.service;
 
+
+import com.platzi.facade.MensajeFacadeImpl;
+import com.platzi.model.Mensaje;
+import java.util.Scanner;
+
 /**
  *
  * @author lmolina
  */
 public class MensajeService {
-    public static void  crearMensaje(){}
-    public static void  listarMensaje(){}
-    public static void  borrarMensaje(){}
-    public static void  actualizarMensaje(){}
+    private static MensajeFacadeImpl mensajeFacade = new MensajeFacadeImpl();
+    public static Scanner sc = new Scanner(System.in);
+    public static void  crearMensaje(){
+        
+        System.out.println("Escribe tu mensaje");
+        String mensaje = sc.nextLine();
+        
+        System.out.println("Escribe tu nombre");
+        String nombre = sc.nextLine();
+        
+        System.out.println("Escribe la fecha");
+        String fecha = sc.nextLine();
+        
+        Mensaje registro = new Mensaje();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+        registro.setFecha_mensaje(fecha);
+      
+//        MensajeFacadeImpl mensajeFacade = new MensajeFacadeImpl();
+        mensajeFacade.crearMensajeDB(registro);
+        
+    }
+    public static void  listarMensaje(){
+        mensajeFacade.leerMensajesDB();
+    }
+    public static void  borrarMensaje(){
+        System.out.println("INSERTAR ID");
+        int idToDelete = sc.nextInt();
+        mensajeFacade.borrarMensajeDB(idToDelete);
+    }
+    public static void  actualizarMensaje(){
+        System.out.println("NUEVO MENSAJE");
+        String nuevoMensaje = sc.nextLine();
+        System.out.println("ID DE MENSAJE");
+        int id = sc.nextInt();
+        Mensaje mensaje = new Mensaje();
+        mensaje.setMensaje(nuevoMensaje);
+        mensaje.setId_mensaje(id);
+        mensajeFacade.actualizarMensajeDB(mensaje);
+    }
 }
